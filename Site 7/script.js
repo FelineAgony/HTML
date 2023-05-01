c5c8b907b4b4a40e26356e2df935403e2070ed30
FelineAgony <deniadenia2@gmail.com>
2023-05-01 19:57:34 +0300

    FelineAgony
    Added comments to all sites

--- Site 7/script.js
+++ Site 7/script.js
@@ -1,16 +1,22 @@
 const crawl = document.querySelector('.crawl');
+// Получение высоты элемента контейнера ".star-wars"
 const containerHeight = document.querySelector('.star-wars').offsetHeight;
+// Получение высоты элемента "crawl"
 const crawlHeight = crawl.offsetHeight;
 
+// Определение функции animate
 function animate() {
+  // Установка начального положения элемента "crawl"
 crawl.style.top = containerHeight * 2.2 + 'px';
+// Использывание библиотеки TweenMax для анимирования элемента "crawl"
   TweenMax.to(crawl, 60, {
     top: -crawlHeight + 'px',
     ease: Linear.easeNone,
     onComplete: function() {
+      // Вызов функции animate снова, когда анимация завершится
       animate();
     }
   });
 }
-
+// Вызов функции animate для запуска анимации
 animate();
